f32fc907266953b6fc21d99b108657af0379893e
bruhmanov <haib_kam@mail.ru>
2025-02-26 20:37:12 +0300

    empty comfig

---
+++ config.py
@@ -0,0 +1,17 @@
+TELEGRAM_BOT_TOKEN = ''
+
+GOOGLE_SHEETS_CREDENTIALS = {
+  "type": "",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "telegram-bot-service-account@neshk-bot-sheet.iam.gserviceaccount.com",
+  "client_id": "",
+  "auth_uri": "",
+  "token_uri": "",
+  "auth_provider_x509_cert_url": "",
+  "client_x509_cert_url": "",
+  "universe_domain": ""
+}
+
+GOOGLE_SHEETS_NAME = ''
